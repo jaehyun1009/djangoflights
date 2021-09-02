2be5125f5dadba1a479cc766bec161657b68cd5a
jaehyun1009 <j.jaehuyn.1009@gmail.com>
2021-09-02 10:05:02 -0700

    finish authentication

--- main_app/urls.py
+++ main_app/urls.py
@@ -2,5 +2,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-  path('', views.Home.as_view(), name='home')
+  path('', views.Home.as_view(), name='home'),
+  path('search/', views.search, name='search'),
+  path('airports/', views.airports_index, name='airports_index'),
+  path('accounts/signup/', views.signup, name='signup')
 ]
--- main_app/views.py
+++ main_app/views.py
@@ -1,5 +1,34 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth import login
+from django.contrib.auth.decorators import login_required
 from django.contrib.auth.views import LoginView
+from django.contrib.auth.forms import UserCreationForm
 
 class Home(LoginView):
   template_name = 'home.html'
+
+def search(request):
+  return render(request, 'search.html')
+
+@login_required
+def airports_index(request):
+  return render(request, 'airports/index.html')
+
+def signup(request):
+  error_message = ''
+  if request.method == 'POST':
+    # This is how to create a 'user' form object
+    # that includes the data from the browser
+    form = UserCreationForm(request.POST)
+    if form.is_valid():
+      # This will add the user to the database
+      user = form.save()
+      # This is how we log a user in
+      login(request, user)
+      return redirect('airports_index')
+    else:
+      error_message = 'Invalid sign up - try again'
+  # A bad POST or a GET request, so render signup.html with an empty form
+  form = UserCreationForm()
+  context = {'form': form, 'error_message': error_message}
+  return render(request, 'signup.html', context)
