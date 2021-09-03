3f27981c05b31ab56b3a57e33d438b2f3d1827d2
jaehyun1009 <j.jaehuyn.1009@gmail.com>
2021-09-03 10:22:45 -0700

    create model

--- main_app/models.py
+++ main_app/models.py
@@ -1,6 +1,43 @@
 from django.db import models
+from django.urls import reverse
 from django.contrib.auth.models import User
 
-# Create your models here.
+CLASSES = (
+  ('E', 'Economy'),
+  ('B', 'Business'),
+  ('F', 'First Class')
+)
+
+class Airport(models.Model):
+  name = models.CharField(max_length=100)
+  code = models.CharField(max_length=3)
+  iso = models.CharField(max_length=2)
+  lat = models.DecimalField(decimal_places=6, max_digits=10)
+  lon = models.DecimalField(decimal_places=6, max_digits=10)
+
+  def __str__(self):
+    return self.code
+
+  def get_absolute_url(self):
+    return reverse('airports_detail', kwargs={'airport_id': self.id})
+
 class Profile(models.Model):
   user = models.OneToOneField(User, on_delete=models.CASCADE)
+  airport = models.ManyToManyField(Airport)
+
+class Ticket(models.Model):
+  price = models.IntegerField()
+  seat_class = models.CharField(
+    max_length=1,
+    choices=CLASSES,
+    default=CLASSES[0][0]
+  )
+  origin = models.ManyToManyField(Airport, related_name='origin_airport')
+  destination = models.ManyToManyField(Airport, related_name='destination_airport')
+  profile = models.ForeignKey(Profile, on_delete=models.CASCADE)
+
+  def __str__(self):
+    return f'Ticket from {self.origin.code} to {self.destination.code}'
+
+  def get_absolute_url(self):
+    return reverse('tickets_detail', kwargs={'ticket_id': self.id})
--- main_app/views.py
+++ main_app/views.py
@@ -3,12 +3,23 @@ from django.contrib.auth import login
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth.views import LoginView
 from django.contrib.auth.forms import UserCreationForm
+from .forms import AirportForm
 
 class Home(LoginView):
   template_name = 'home.html'
 
 def search(request):
-  return render(request, 'search.html')
+  return render(request, 'search.html', {'form': AirportForm()})
+
+def get_airport(request):
+  if request.method == 'POST':
+    form = AirportForm(request.POST)
+    print(form)
+    if form.is_valid():
+      return render(request, 'search.html', {'form': form})
+  else:
+    form = AirportForm()
+  return render(request, 'search.html', {'form': form})
 
 @login_required
 def airports_index(request):
